587181f80c0e570a06757bb7e345e8e7dbab5904
ÁLVARO LLAMAS HUERTA <alvaro.llahue@educa.jcyl.es>
2024-11-20 12:09:35 +0100

    20/11/2024

--- 306_AWgestion_alumnos_proyectoPDO/controlador/agregar_proyecto.php
+++ 306_AWgestion_alumnos_proyectoPDO/controlador/agregar_proyecto.php
@@ -31,16 +31,28 @@
         die("Error al preparar la consulta. " . $mysqli_conexion -> error);
     }
 
-    // Vincular parámetros usando bindParam
-    $titulo = "ComidaSana";
-    $logotipo = file_get_contents(filename: "https://cdn.pixabay.com/photo/2017/06/29/20/16/food-2456100_640.jpg");
-
+    // Vinculamos parámetros usando bindParam
     $sentencia -> bindParam(param: ':titulo', var: $titulo, type: PDO::PARAM_STR);
+    $sentencia -> bindParam(param: ':descripcion', var: $descripcion, type: PDO::PARAM_STR);
+    $sentencia -> bindParam(param: ':periodo', var: $periodo, type: PDO::PARAM_INT);
+    $sentencia -> bindParam(param: ':curso', var: $curso, type: PDO::PARAM_INT);
+    $sentencia -> bindParam(param: ':fecha_presentacion', var: $fecha_presentacion, type: PDO::PARAM_STR);
+    $sentencia -> bindParam(param: ':nota', var: $nota, type: PDO::PARAM_INT);
     $sentencia -> bindParam(param: ':logotipo', var: $logotipo, type: PDO::PARAM_LOB);
 
     // Ejecutamos la consulta
-    $sentencia -> execute();
+    if($sentencia -> execute()){
 
-    echo "¡Datos insertados!";
+        // Redirigimos al listado de proyectos
+        header("Location: ../vista/listar_proyecto.php");
+        exit;
+
+    } else {
+        echo "Error al insertar el registro " . $mysqli_conexion -> error;
+    }
+
+    // Cerramos la sentencia y desconectamos
+    $sentencia -> close();
+    $mysqli_conexion -> close();
 
 ?>
